cf843a4969f770ee82d0fda29388795a47446dde
marimonon <mappi-@hotmail.co.jp>
2024-11-14 15:23:11 +0900

    17歳でストップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,8 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          onClick={() => setCount((count) => (count < 17 ? count + 1 : count))}
+        >
+          Age is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
